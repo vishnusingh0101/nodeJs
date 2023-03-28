02a9ae5b78de1d3e36a4cc3118653d9c11d3356c
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-03-28 10:40:15 +0530

    changes according to url

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log('Vishnu Singh');
+    const url = req.url;
+    if(url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>About</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>');
+        res.end();
+    }else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>Node</title></head>');
+        res.write('<body><h1> Welcome to my Node Js project</h1></body>')
+        res.write('</html>');
+        res.end();
+    }
+    
+    console.log(req.headers);
 });
 
 server.listen(4000);
