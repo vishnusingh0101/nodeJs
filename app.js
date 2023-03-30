037482bea2825a6e95c2214111e3647cfba6e398
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-03-30 20:02:33 +0530

    added one more input for size

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((_req, _res, next) => {
-    console.log('In the middle ware');
-    next();
-});
-app.use((_req, _res) => {
-    console.log('In the middleware');
-    _res.send('<h1>hello to node js</h1>')
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (_req, _res) => {
+    _res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button><form/>')
     // ...
 });
+
+app.post('/product', (_req, _res, next)=>{
+    console.log(_req.body);
+    _res.redirect('/');
+});
+
+app.use('/', (_req, _res, next) => {
+    _res.send('<h1>This is it</h1>')
+});
+
 
 
 app.listen(4000); 
